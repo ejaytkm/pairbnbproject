a7f3781f5be3137d2c12f87f2f78e0f28a0692bf
ejaytkm@gmail.com <ejaytkm@gmail.com>
2016-11-24 15:40:33 +0800

    Small authorizations for editing the functions : 1) show     - tenant, landlord, admin 2) edit     - landlord, admin 3) destroy  - landlord, admin
    Next step: Landlord are only allowed to edit and destroy their own listings
    Note: Admin can prolly do both
    
    Follow up: Create a listing belongs to users that registered as tenants!
    So technically, many listings belong to one user.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161123031633) do
+ActiveRecord::Schema.define(version: 20161124032315) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20161123031633) do
     t.string   "encrypted_password", limit: 128
     t.string   "confirmation_token", limit: 128
     t.string   "remember_token",     limit: 128
+    t.string   "roles"
     t.index ["email"], name: "index_users_on_email", using: :btree
     t.index ["remember_token"], name: "index_users_on_remember_token", using: :btree
   end
