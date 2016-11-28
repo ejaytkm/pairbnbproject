f4ec4bf037e746a6f8c28feb3f0ab3513aa19280
ejaytkm@gmail.com <ejaytkm@gmail.com>
2016-11-28 14:23:12 +0800

    stuff

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,9 @@
 class HomeController < ApplicationController
-  def index
-  end
-
-  def new
-  end
+	
+	def index 
+		if current_user && current_user.profile.nil? 
+			profile = Profile.new(user_id: current_user.id)
+		end
+	end 
 
 end
